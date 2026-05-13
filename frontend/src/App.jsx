d7a5763b01b768fcec8c569f3f6f0aa59d7aabf9
iree26 <oyetiboire@gmail.com>
2026-05-13 23:43:16 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './components/Landing'
+import OnboardingForm from './components/OnboardingForm'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/onboarding" element={<OnboardingForm />} />
       </Routes>
     </BrowserRouter>
   )
